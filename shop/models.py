55948b7691d47426581e3c042b19b7d7103b7cdb
Z <db.emad@gmail.com>
2024-02-12 01:12:53 +0330

    first faceless

---
+++ shop/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'    
+       
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=500, default='', blank=True, null=True)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=12)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)    
+    picture = models.ImageField(upload_to='upload/product/')
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)    
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address= models.CharField(max_length=500, default='', blank=True)
+    phone = models.CharField(max_length=400, default='', blank=True)
+    date = models.DateField(default=timezone.now)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
---
+++ shop/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def helloworld(request):
+    return render(request, 'index.htm')
+
